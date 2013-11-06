bf892d1702e10632ddde2eb1e86716b9aa82f39d
liubo <shenxian@163.com>
2013-11-06 17:54:11 +0800

    初始化

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('MyAngularJsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
